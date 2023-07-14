121e4776c2166eec4cb9720427d846ba9a9103c9
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2023-07-14 23:53:16 +0000

    simplify the code

--- UniqueNumberofOccurrencesCpp.cpp
+++ UniqueNumberofOccurrencesCpp.cpp
@@ -10,12 +10,13 @@ using namespace std;
 bool isFrequencyUnique(int n, int arr[])
 {
 	unordered_map<int, int> vals{};
-	unordered_set<int> uniqueFrequency{};
+
 	for (int i = 0; i < n; i++)
 	{
 		int current = arr[i];
 		vals[current]++;
 	}
+	unordered_set<int> uniqueFrequency{};
 	for (const auto &pair : vals)
 	{
 		if (uniqueFrequency.count(pair.second) > 0)
@@ -36,14 +37,3 @@ int main()
 	isFrequencyUnique(9, arr);
 	std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
